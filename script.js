0c0c5f0fde12df847c110d7af7187d302fd16d88
sebastienmariette74 <61980470+sebastienmariette74@users.noreply.github.com>
2022-03-18 10:57:18 +0100

    travail sur les parties chronométrées

--- script.js
+++ script.js
@@ -2,14 +2,19 @@
 
 $(function(){
   $('.modal').css('display','none');
+  $('.modal1').css('display','none');
   let start = $('.button_start');
-  console.log(start);
-  let restart = $('.button_restart');
+  let submit = $('.submit');
   start.css('cursor', 'pointer');
-  restart.css('cursor', 'pointer');
+  $('.restart').css('cursor', 'pointer');
+  submit.css('cursor', 'pointer');
   let progressBar = $('#MaBarreProgression');
   progressBar.css('display', 'none');
-  // progressBar.progressbar("disabled");
+  let score;
+  let bestScore = 12000;
+  let bestScoreDisplay;
+  let time, radio;
+  let clicForbidden = false;
 
   /* ------------------ le bouton start clignote ------------------- */
   let bindStart;
@@ -67,61 +72,51 @@ $(function(){
   let table1 = $('.box');
   let boxes = [...table1];
 
-  start.click(()=>{
-    $('body').css('backgroundColor','rgba(0, 0, 0, 0)');
 
-    /* ---------- disposition des figurines dans le tableau ----------------- */  
-    for (let box of boxes) {
-      // box.style.backgroundColor = '#fffaba';
-      box.style.backgroundColor = '#ffffff';
-      box.style.borderColor = '#ffffff';
-      let numberOfRemainingBoxes = animals.length; 
-      let randomNumber = function() { 
-        return Math.floor(Math.random() * (numberOfRemainingBoxes-1));
-      };        
-      let nb = randomNumber();
-      box.innerHTML = animals[nb];
-
-      if (box.hasChildNodes()) {
-        let children = box.childNodes;
-      
-        for (let child of children) {
-          child.style.opacity = '0';
-        }
-      }       
-      animals.splice(nb, 1);
-      clearInterval(bindStart);
-    }; 
-    /* _____________________________________________________________________ */
-
-    start.prop('disabled', true);
-    progressBar.css('display', 'block');
-
-
-    /* --------------------- BARRE DE PROGRESSION ------------------------------- */
-    let max = progressBar.attr('max');
-    let time = 1;
-    let value = progressBar.val();
-
-    let Telechargement = ()=> {
-        value += 1;
-        addValue = progressBar.val(value);
-
-        $('.progress-value').html('Téléchargement '+ value + '%');
-
-        if (value == max) {
-            clearInterval(animation);   
-            $('.progress-value').html('Téléchargement Terminé !');                 
-        }
-    };
-
-    let animation = setInterval(()=> {
-        Telechargement();
-    }, time);
-  /* _________________________________________________________________________________*/
-  })
+  /* --------------------- BARRE DE PROGRESSION ------------------------------- */   
   
+  let value=0, max;
+
+  function chrono (sec){
+    let time = 10;
+    let chronometer = ()=> {
+      progressBar.attr('max',sec);
+      max = progressBar.attr('max');
+      if (!endGame){
+        value += 1;
+      } else {
+        clearInterval(animation);   
+      }
+      // addValue = progressBar.val(value);
+      progressBar.val(value);
+      if (value == max) {
+        endGame = true;
+        clearInterval(animation);   
+        $('.modal_text').html('Partie perdue !');  
+
+        $('.modal1').css('display','block');    
+
+      }
+    };
+  
+    let animation = setInterval(()=> {
+        chronometer();
+    }, time);
+  }
+  
+  /* _________________________________________________________________________________*/  
+
+
+
+  /* ----------------   START ----------------------------------------------- */
+  start.click(()=>{
+    $('section').css('backgroundColor','rgba(0, 0, 0, 0)');
+    $('.modal').css('display','block');
+    clearInterval(bindStart);
+    start.prop('disabled', true);
+  })  
   /* ________________________________________________________________________ */
+
 
   /* ----------------------------------- action lorsqu'on clique sur une case ------------------------------*/
   let nbClic = 1;
@@ -131,18 +126,11 @@ $(function(){
   let endGame = false;
   let clic = (event) => {
     event.addEventListener('click', (e)=>{
-      // console.log(event.style.opacity);
       let children = event.childNodes;      
       for (let child of children) {
-        console.log(child.style.opacity);
-        if (child.style.opacity === '1'){
+        if (child.style.opacity === '1' || value === max || clicForbidden || endGame){
           e.preventDefault();
-          console.log('if');
           } else if (nbClic < 2){
-
-            console.log('else if');
-            console.log(nbClic);      
-
             nbClic++;
             if (event.hasChildNodes()) {
               let children = event.childNodes;      
@@ -153,8 +141,6 @@ $(function(){
               }
             };
           } else {
-            console.log('else');
-            console.log(nbClic);      
             nbClic = 1;
             if (event.hasChildNodes()) {
               let children = event.childNodes;      
@@ -162,43 +148,89 @@ $(function(){
                 child.style.opacity = '1';
                 secondPicture = child;
                 secondPictureAttribute = child.getAttribute('src');
-
               }
             };
 
             if(firstPictureAttribute === secondPictureAttribute){
-              console.log('ok');
               foundPairs++;
-              console.log(foundPairs);
               if (foundPairs === 8){
                 endGame = true;
-                console.log('gagné');
-                $('.modal').css('display','block');
-
+                score = value;
+                scoreDisplay = parseInt(value/100) + '"' + Math.round(((value/100)-parseInt(value/100))*100) + '.';
                 
 
+                if (radio === 'oui'){
+                  if (score < bestScore){
+                    bestScore = score;
+                    bestScoreDisplay = "c'est ton meilleur temps.";
+                    $('.modal_text').html('BRAVO !!! Tu as mis ' + scoreDisplay + bestScoreDisplay);  
+                  } else {
+                    // bestScoreDisplay = "Ton meilleur temps est de " + bestScore + '.';
+                    $('.modal_text').html('BRAVO !!! Tu as mis ' + scoreDisplay + '.');  
+                  };
+                } else {
+                  $('.modal_text').html('BRAVO !!!');  
+                }
+                $('.modal1').css('display','block');
+                progressBar.css('display','none');
               }
-
             } else {
-              console.log('non');
+              clicForbidden = true;
               setTimeout(()=>{
                 firstPicture.style.opacity ='0';
-                secondPicture.style.opacity ='0';            
+                secondPicture.style.opacity ='0';          
+                clicForbidden = false;  
               },1000);
             };
         };
       };
     });
-    console.log(nbClic);
   };
 
   for (let box of boxes){
     box.style.cursor = 'pointer';
     clic(box);
   };
+  /*__________________________________________________________________________________________________________*/  
 
-  restart.click(()=>{
-    /* -------------- REINITITIALISATION DE LA GRILLE ----------------------------*/
+  
+  
+  /* --------------------------------------- BOUTON OK ----------------------------- */
+  submit.click(()=>{
+    endGame = false;
+    value = 0;
+    progressBar.val(value);
+    time = $('select option:selected').text();
+    radio = $('input[name=chrono]:checked').val();
+    if (radio === 'oui'){
+      if (time === '30s'){
+        let sec = 500;
+        progressBar.css('display', 'block');
+        $('.modal1').css('display', 'none');
+        chrono(sec);
+      } else if (time === '60s'){
+        let sec = 6000;
+        progressBar.css('display', 'block');
+        $('.modal1').css('display', 'none');
+        chrono(sec);
+      } else if (time === '90s'){
+        let sec = 9000;
+        progressBar.attr('max','900');
+        progressBar.css('display', 'block');
+        $('.modal1').css('display', 'none');
+        chrono();
+      } else {
+        let sec = 12000;
+        progressBar.css('display', 'block');
+        $('.modal1').css('display', 'none');
+        chrono();
+      } 
+    } else {
+      progressBar.css('display', 'none');
+      $('.modal1').css('display', 'none');
+    };
+
+    /* -------------- INITITIALISATION DE LA GRILLE ----------------------------*/
     let animals = [cow, elephent, herisson, hippo, penguin, shark, snake, tortle, cow, elephent, herisson, hippo, penguin, shark, snake, tortle];
     let table1 = $('.box');
     let boxes = [...table1];
@@ -206,7 +238,7 @@ $(function(){
     for (let box of boxes) {
       let numberOfRemainingBoxes = animals.length; 
       let randomNumber = function() { 
-        return Math.floor(Math.random() * (numberOfRemainingBoxes-1));
+        return Math.floor(Math.random() * (numberOfRemainingBoxes));
       };        
       let nb = randomNumber();
       box.innerHTML = animals[nb];
@@ -225,86 +257,16 @@ $(function(){
     $('.modal').css('display','none');
     foundPairs = 0;
   })
+  /* __________________________________________________________________________ */
 
-//   /* --------------------- BARRE DE PROGRESSION ------------------------------- */
-//   // let MaBarreProgression = $('#MaBarreProgression');
-//   // let max = MaBarreProgression.attr('max');
-//   // let time = 1;
-//   // let value = MaBarreProgression.val();
 
-//   // let Telechargement = ()=> {
-//   //     value += 1;
-//   //     addValue = MaBarreProgression.val(value);
+  /* --------------------------------- BOUTON RESTART --------------------------*/
+  $('.restart').click(()=>{
+    $('.modal').css('display','block');
+  });
+  /* _____________________________________________________________________________*/
 
-//   //     $('.progress-value').html('Téléchargement '+ value + '%');
-
-//   //     if (value == max) {
-//   //         clearInterval(animation);   
-//   //         $('.progress-value').html('Téléchargement Terminé !');                 
-//   //     }
-//   // };
-
-//   // let animation = setInterval(()=> {
-//   //     Telechargement();
-//   // }, time);
-//   /* _________________________________________________________________________________*/
-
-// /* --------------------- BARRE DE PROGRESSION ------------------------------- */
-// let max = progressBar.attr('max');
-// let time = 1;
-// let value = progressBar.val();
-
-// let Telechargement = ()=> {
-//     value += 1;
-//     addValue = progressBar.val(value);
-
-//     $('.progress-value').html('Téléchargement '+ value + '%');
-
-//     if (value == max) {
-//         clearInterval(animation);   
-//         $('.progress-value').html('Téléchargement Terminé !');                 
-//     }
-// };
-
-// let animation = setInterval(()=> {
-//     Telechargement();
-// }, time);
-// /* _________________________________________________________________________________*/
-
-//   function init(){ // four progress bars 
-//     $("#pbar0").progressbar({ "value": 63 }); 
-//     $("#pbar1").progressbar({ "value": 47 }); 
-//     $("#pbar2").progressbar({ "value": 33 }); 
-//     $("#pbar3").progressbar({ "value": 21 }); 
-//     // the zero'th progressbar gets the default theme 
-//     // set colors for progressbar #1 
-//     $("#pbar1").css({ 'background': 'url(images/white-40x100.png) #ffffff repeat-x 50% 50%;' }); 
-//     $("#pbar1 > div").css({ 'background': 'url(images/lime-1x100.png) #cccccc repeat-x 50% 50%;' }); 
-//     // set colors for progressbar #2 
-//     $("#pbar2").css({ 'background': 'url(images/lt-blue-40x100.png) #ffffff repeat-x 50% 50%' }); 
-//     $("#pbar2 > div").css({ 'background': 'url(images/dustyblue-1x100.png) #cccccc repeat-x 50% 50%' }); 
-//     // set colors for progressbar #3 
-//     $("#pbar3").css({ 'background': 'LightYellow' }); 
-//     $("#pbar3 > div").css({ 'background': 'Orange' }); 
-//   };
-
-//   $("#pbar0").bind('progressbarchange', function(event, ui) { 
-//     var selector = "#" + this.id + " > div"; 
-//     var value = this.getAttribute( "aria-valuenow" ); 
-//     if (value < 10){ 
-//       $(selector).css({'background': 'Red' }); 
-//     } else if (value < 30){ 
-//       $(selector).css({ 'background': 'Orange' 
-//     }); 
-//     } else if (value < 50){ 
-//       $(selector).css({ 'background': 'Yellow' 
-//     }); 
-//     } else{ 
-//       $(selector).css({ 'background': 'LightGreen' 
-//       }); 
-//     } 
-//   });
-
+  
 
 
 }); /* fin du code */
